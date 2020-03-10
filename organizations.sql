07749112f67fe073ca4f572ddb43fbd55968c1b9
Aaron Neslin <aneslin125@gmail.com>
2020-03-10 14:34:16 -0400

    Update organizations.sql

--- organizations.sql
+++ organizations.sql
@@ -24,10 +24,10 @@ group by org2.organizationID) as Aliases
 from
 coral_organizations.Organization org
 Left join coral_organizations.Alias alcode
-#type 5 is distinct to UMASS.  Type may be different for other schools
+#type 5 (vendor code) is distinct to UMASS.  Type may be different for other schools
 on org.organizationID = alcode.organizationID and alcode.aliasTypeID = 5
 left join coral_organizations.OrganizationRoleProfile orp1
-#role 6 is distinct to UMASS. RoleID might be different for other schools
+#role 6 (is vendor) is distinct to UMASS. RoleID might be different for other schools
 on org.organizationID =  orp1.organizationID and orp1.organizationRoleID = 6
 left join coral_organizations.OrganizationRoleProfile orp2
 on org.organizationID = orp2.organizationID
