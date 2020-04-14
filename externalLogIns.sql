d458583a881317f0eaea257b8c7b346aa15c0445
Aaron Neslin <aneslin125@gmail.com>
2020-04-14 11:17:50 -0400

    added log in urls

--- externalLogIns.sql
+++ externalLogIns.sql
@@ -1,7 +1,11 @@
-select a.name as code, group_concat(ext.shortName) as interfaceType ,ex.emailAddress, ex.username, md5(ex.password), 
+select a.name as code, group_concat(ext.shortName) as interfaceType ,
+ex.emailAddress, 
+ex.username, md5(ex.password), 
+ex.loginURL,
 RePLACE(REPLACE(ex.noteText, ',',';'), '\n', ' ') as Notes
  from coral_organizations.ExternalLogin ex
 inner join coral_organizations.Alias a
+#when running smith, update alias type
 on  ex.organizationID = a.organizationID and a.aliasTypeID = 5
 inner join coral_organizations.ExternalLoginType ext
 on ex.externalLoginTypeID = ext.externalLoginTypeID
