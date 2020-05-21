efe64509c891d91e37508ad3af36e74639cbd3aa
Aaron Neslin <aneslin125@gmail.com>
2020-05-21 12:53:21 -0400

    Create resources.sql

---
+++ resources.sql
@@ -0,0 +1,34 @@
+SELECT distinct al.name as orgCode , r.titleText, r.descriptionText, 
+r.orderNumber, r.systemNumber, r.currentStartDate, 
+r.currentEndDate , at.shortName as acqType, rt.shortName as resourceType, st.shortName as statusName,
+(SELECT group_concat(atyr.shortName, ': ' ,alr.shortName)
+FROM 
+coral_resources.Alias alr
+inner join
+coral_resources.AliasType atyr 
+on alr.aliasTypeID = atyr.aliasTypeID
+inner join 
+coral_resources.Resource r2
+on alr.resourceID = r2.resourceID
+where r2.resourceID = r.resourceID
+group by r2.resourceID) as alias 
+
+FROM coral_resources.Resource r
+left join coral_resources.ResourceOrganizationLink rl
+on r.resourceID = rl.resourceID
+left  join  coral_organizations.Organization org
+on rl.organizationID = org.organizationID and rl.organizationRoleID = 6
+inner join coral_organizations.Alias al 
+on rl.organizationID = al.organizationID and al.aliasTypeID = 5
+inner join coral_resources.Status st
+on r.statusID = st.statusID
+inner join coral_resources.ResourceType rt
+on r.resourceTypeID = rt.resourceTypeID
+inner join coral_resources.AcquisitionType at
+on r.acquisitionTypeID = at.acquisitionTypeID
+where
+st.statusID != 4
+and rt.resourceTypeID not In (3, 5)
+and r.resourceID not in (select resourceID from coral_resources.ResourceRelationship)
+and r.acquisitionTypeID not in (20, 19, 2, 8, 9, 12, 25,23,24,15)
+
