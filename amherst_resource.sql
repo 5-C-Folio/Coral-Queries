0174bb1cb70a3dd8694a8801f47658b946b5bef4
Aaron Neslin <aneslin125@gmail.com>
2020-07-22 14:49:27 -0400

    Create amherst_resource.sql

---
+++ amherst_resource.sql
@@ -0,0 +1,36 @@
+SELECT distinct r.resourceID, al.name as orgCode, r.titleText, replace(replace(r.descriptionText,'\n',''), ',',';'), 
+r.orderNumber, r.systemNumber, r.currentStartDate, 
+r.currentEndDate , at.shortName as acqType, rt.shortName as resourceType, st.shortName as statusName,
+(SELECT group_concat(atyr.shortName, ': ' ,alr.shortName)
+FROM 
+coral_resources.Alias alr
+inner join
+coral_resources.AliasType atyr 
+on alr.aliasTypeID = atyr.aliasTypeID
+inner join 
+coral_resources.Resource r2
+on alr.resourceID = r2.resourceID
+where r2.resourceID = r.resourceID
+group by r2.resourceID) as alias 
+
+FROM coral_resources.Resource r
+left join coral_resources.ResourceOrganizationLink rl
+on r.resourceID = rl.resourceID
+left  join  coral_organizations.Organization org
+on rl.organizationID = org.organizationID 
+left join coral_organizations.Alias al 
+on rl.organizationID = al.organizationID and al.aliasTypeID = 5
+left join coral_resources.Status st
+on r.statusID = st.statusID
+left join coral_resources.ResourceType rt
+on r.resourceTypeID = rt.resourceTypeID
+left join coral_resources.AcquisitionType at
+on r.acquisitionTypeID = at.acquisitionTypeID
+where
+st.statusID != 4
+and  rl.organizationRoleID = 6
+and r.resourceTypeID in (2,3,5,7,9,10)
+and r.resourceID not in (select resourceID from coral_resources.ResourceRelationship)
+and r.resourceID not in (select relatedResourceID from coral_resources.ResourceRelationship)
+and al.aliasID != 227
+and r.acquisitionTypeID NOT IN (6, 2, 9, 11, 12)
